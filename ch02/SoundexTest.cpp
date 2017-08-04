2b931d9b0f614c4b0c8b55951832b41aafdc2edf
Josh Deaton <jdeaton@users.noreply.github.com>
2017-08-04 18:59:00 -0400

    Incorporated first fixture. Fixture for SoundexEncoding test cases.
    This eliminates the need to create a Soundex object inside of each test in a set of test cases.

--- ch02/SoundexTest.cpp
+++ ch02/SoundexTest.cpp
@@ -15,16 +15,17 @@ private:
     }
 };
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+class SoundexEncoding: public testing::Test {
+public:
     Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     auto encoded = soundex.encode("A");
     ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-    Soundex soundex;
-
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
     auto encoded = soundex.encode("I");
-
     ASSERT_THAT(encoded, Eq("I000"));
 }

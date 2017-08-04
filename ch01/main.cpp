6e1143fffe489cba31178084e7e16ccd32b102f0
Josh Deaton <jdeaton@users.noreply.github.com>
2017-08-04 17:51:04 -0400

    Chapter 1 files that ensure I have functional Google Mock/Test.
    Currenty using a build of Google Mock/Test located outside of the project. According to CLion help I really want to have these things inside of my project. I will figure out how to do this later.

--- ch01/main.cpp
+++ ch01/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// Some dummy includes from Google Test/Mock to see if CLion is finding them.
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;

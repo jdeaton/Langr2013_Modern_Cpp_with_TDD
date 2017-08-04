f2d4208d702548904e28d977a437ee9fee998c27
Josh Deaton <jdeaton@users.noreply.github.com>
2017-08-04 18:52:33 -0400

    Added a second test for Soundex.encode() for similar functionality.

--- ch02/SoundexTest.cpp
+++ ch02/SoundexTest.cpp
@@ -1,17 +1,30 @@
 #include <gmock/gmock.h>
 
+using std::string;
 using testing::Eq;
 
-class Soundex
-{
+class Soundex {
 public:
-    std::string encode(const std::string& word) const {
-        return "A";
+    string encode(const string& word) const {
+        return zeroPad(word);
+    }
+
+private:
+    string zeroPad(const string& word) const {
+        return word + "000";
     }
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
     Soundex soundex;
     auto encoded = soundex.encode("A");
-    ASSERT_THAT(encoded, Eq("A"));
+    ASSERT_THAT(encoded, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    Soundex soundex;
+
+    auto encoded = soundex.encode("I");
+
+    ASSERT_THAT(encoded, Eq("I000"));
 }

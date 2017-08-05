b192f2504e2f8f4eb6006e72d01b87146bf57330
Josh Deaton <jdeaton@users.noreply.github.com>
2017-08-05 14:03:26 -0400

    Refactored Sounded.encode() functionality to be more expressive.

--- ch02/Soundex.h
+++ ch02/Soundex.h
@@ -8,18 +8,20 @@ using namespace std;
 class Soundex {
 public:
     string encode(const string& word) const {
-        auto encoded = word.substr(0, 1);
-
-        if (word.length() > 1)
-        {
-            encoded += "1";
-        }
-
-
-        return zeroPad(encoded);
+        return zeroPad(head(word) + encodedDigits(word));
     }
 
 private:
+    string head(const string& word) const {
+        // Currently gets/returns first character in word.
+        return word.substr(0, 1);
+    }
+
+    string encodedDigits(const string &word) const {
+        if (word.length() > 1) return "1";
+        return "";
+    }
+
     string zeroPad(const string& word) const {
         auto zerosNeeded = 4 - word.length();
         return word + string(zerosNeeded, '0');

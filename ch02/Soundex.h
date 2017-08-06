3a49500c3bcff351cc20fa91637512c8da0ec747
Josh Deaton <jdeaton@users.noreply.github.com>
2017-08-06 15:28:18 -0400

    Completed Soundex encoding for all alphabetics in the second character.
    Also incorporated functionality and testing for non-alphabetics.

--- ch02/Soundex.h
+++ ch02/Soundex.h
@@ -2,8 +2,11 @@
 #define Soundex_h
 
 #include <string>
+#include <unordered_map>
 
 using namespace std;
+
+static const size_t MAX_CODE_LENGTH{4};
 
 class Soundex {
 public:
@@ -18,12 +21,26 @@ private:
     }
 
     string encodedDigits(const string &word) const {
-        if (word.length() > 1) return "1";
+        if (word.length() > 1) return encodedDigit(word[1]);
         return "";
     }
 
+    string encodedDigit(char letter) const {
+        const unordered_map<char, string> encodings {
+                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
+                            {'s', "2"}, {'x', "2"}, {'z', "2"},
+                {'d', "3"}, {'t', "3"},
+                {'l', "4"},
+                {'m', "5"}, {'n', "5"},
+                {'r', "6"}
+        };
+        auto it = encodings.find(letter);
+        return it == encodings.end() ? "" : it->second;
+    }
+
     string zeroPad(const string& word) const {
-        auto zerosNeeded = 4 - word.length();
+        auto zerosNeeded = MAX_CODE_LENGTH - word.length();
         return word + string(zerosNeeded, '0');
     }
 };
